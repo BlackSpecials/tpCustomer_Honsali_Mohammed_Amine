cbb4b5d7712886cdab7caf49cad029da524ca93a
CO1 <honsaliamine04@gmail.com>
2023-10-27 21:35:25 +0100

    Etat initial

---
+++ src/main/java/ma/honsali_mohammed_amine/tp_customer/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package ma.honsali_mohammed_amine.tp_customer;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
